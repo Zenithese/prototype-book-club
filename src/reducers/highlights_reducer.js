91820fd104db811bc1e60cf53b4ccbcd134a196a
jandersen770 <jandersen770@gmail.com>
2020-05-21 15:48:00 -0400

    Change Object.assign to spread operator for array manipulation

--- src/reducers/highlights_reducer.js
+++ src/reducers/highlights_reducer.js
@@ -4,11 +4,11 @@ const highlightsReducer = (state = [], action) => {
     Object.freeze(state);
     switch (action.type) {
         case RECEIVE_HIGHLIGHT:
-            return Object.assign([], state, { [action.highlight.id]: action.highlight })
+            return [...state, action.highlight]
         case RECEIVE_HIGHLIGHTS:
             return action.highlights
         case REMOVE_HIGHLIGHT:
-            let newState = Object.assign([], state)
+            let newState = [...state]
             return newState.filter(highlight => highlight.id !== action.id)
         default:
             return state;
