d28d919d04ce5e88f5988789462f23ade17565e0
jandersen770 <jandersen770@gmail.com>
2020-06-25 20:09:46 -0400

    make frontend/backend, react/redux settings cycle

--- src/actions/settings_actions.js
+++ src/actions/settings_actions.js
@@ -1,12 +1,14 @@
+import * as APIUtil from '../util/session_api_util';
+
 export const STORE_SETTINGS = "STORE_SETTINGS"
 export const RECEIVE_SETTINGS = "RECEIVE_SETTINGS"
 
-const storeSettings = (color, fontSize, theme) => {
+const storeSettings = (settings) => {
     return {
         type: STORE_SETTINGS,
-        color,
-        fontSize,
-        theme,
+        color: settings.highlight_color,
+        fontSize: settings.font_size,
+        theme: settings.theme,
     }
 }
 
@@ -16,8 +18,14 @@ const receiveSettings = () => {
     }
 }
 
+// export const createSettings = (color, fontSize, theme) => dispatch => {
+//     return dispatch(storeSettings(color, fontSize, theme))
+// }
+
 export const createSettings = (color, fontSize, theme) => dispatch => {
-    return dispatch(storeSettings(color, fontSize, theme))
+    return APIUtil.updateSettings(color, fontSize, theme).then(settings => {
+        dispatch(storeSettings(settings))
+    })
 }
 
 export const fetchSettings = () => dispatch => {
--- src/util/session_api_util.js
+++ src/util/session_api_util.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import humps from 'humps';
+var $ = require('jquery');
 
 export const login = user => {
     return axios.post(
@@ -28,6 +29,28 @@ export const signup = user => {
         { withCredentials: true }
     )
 };
+
+// export const updateSettings = (color, fontSize, theme) => {
+//     return axios.put(
+//         'http://localhost:3001/api/users',
+//         { color, fontSize, theme },
+//         {
+//             withCredentials: true,
+//             transformResponse: [
+//                 ...axios.defaults.transformResponse,
+//                 data => humps.camelizeKeys(data)
+//             ],
+//         }
+//     )
+// }
+
+export const updateSettings = (color, fontSize, theme) => {
+    return $.ajax({
+        method: 'PATCH',
+        url: 'http://localhost:3001/api/users/1',
+        data: { user: { highlight_color: color, font_size: fontSize, theme } },
+    });
+}
 
 export const logout = () => {
     return axios.delete(
